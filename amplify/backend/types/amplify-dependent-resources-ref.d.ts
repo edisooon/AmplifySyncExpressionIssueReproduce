db2783b17b846a7ccb84f209f914087aff482dc4
Edison Zhang <edisonzz@amazon.com>
2024-09-27 14:08:26 -0700

    use amplify push to sync AppSync with DataStore

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- app/src/main/java/com/example/taskthreereproduce/MyAmplifyApp.kt
+++ app/src/main/java/com/example/taskthreereproduce/MyAmplifyApp.kt
@@ -3,6 +3,7 @@ package com.example.taskthreereproduce
 import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.api.aws.AWSApiPlugin
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.datastore.AWSDataStorePlugin
 
@@ -11,6 +12,7 @@ class MyAmplifyApp : Application(){
     override fun onCreate() {
         super.onCreate()
         try {
+            Amplify.addPlugin(AWSApiPlugin())
             Amplify.addPlugin(AWSDataStorePlugin())
             Amplify.configure(applicationContext)
 
